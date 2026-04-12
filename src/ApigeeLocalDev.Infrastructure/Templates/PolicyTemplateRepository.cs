39a90f4f5a1446d8c61a7dae7145faf2dd2ab0f3
Marcus Costa <marcusviniciusdefreitascosta@yahoo.com.br>
2026-04-12 20:53:37 -0300

    refactor: use multi-line XML in policy templates and simplify placeholder replacement

--- src/ApigeeLocalDev.Infrastructure/Templates/PolicyTemplateRepository.cs
+++ src/ApigeeLocalDev.Infrastructure/Templates/PolicyTemplateRepository.cs
@@ -12,49 +12,82 @@ public sealed class PolicyTemplateRepository : IPolicyTemplateRepository
             "AssignMessage",
             "Sets or modifies HTTP request/response headers and body.",
             "Mediation",
-            """<AssignMessage name="{{PolicyName}}">\n  <AssignTo createNew=\"false\" type=\"request\"/>\n  <Set>\n    <Headers>\n      <Header name=\"X-Custom-Header\">{{HeaderValue}}</Header>\n    </Headers>\n  </Set>\n</AssignMessage>""",
+            """<AssignMessage name="{{PolicyName}}">
+  <AssignTo createNew="false" type="request"/>
+  <Set>
+    <Headers>
+      <Header name="X-Custom-Header">{{HeaderValue}}</Header>
+    </Headers>
+  </Set>
+</AssignMessage>""",
             ["PolicyName", "HeaderValue"]),
 
         new PolicyTemplate(
             "VerifyAPIKey",
             "Validates API keys in requests.",
             "Security",
-            """<VerifyAPIKey name="{{PolicyName}}">\n  <APIKey ref=\"request.queryparam.apikey\"/>\n</VerifyAPIKey>""",
+            """<VerifyAPIKey name="{{PolicyName}}">
+  <APIKey ref="request.queryparam.apikey"/>
+</VerifyAPIKey>""",
             ["PolicyName"]),
 
         new PolicyTemplate(
             "SpikeArrest",
             "Throttles request rate to protect backend services.",
             "Traffic Management",
-            """<SpikeArrest name="{{PolicyName}}">\n  <Rate>{{Rate}}</Rate>\n</SpikeArrest>""",
+            """<SpikeArrest name="{{PolicyName}}">
+  <Rate>{{Rate}}</Rate>
+</SpikeArrest>""",
             ["PolicyName", "Rate"]),
 
         new PolicyTemplate(
             "OAuthV2-VerifyToken",
             "Validates OAuth 2.0 access tokens.",
             "Security",
-            """<OAuthV2 name="{{PolicyName}}">\n  <Operation>VerifyAccessToken</Operation>\n</OAuthV2>""",
+            """<OAuthV2 name="{{PolicyName}}">
+  <Operation>VerifyAccessToken</Operation>
+</OAuthV2>""",
             ["PolicyName"]),
 
         new PolicyTemplate(
             "ResponseCache",
             "Caches backend responses to reduce latency.",
             "Mediation",
-            """<ResponseCache name="{{PolicyName}}">\n  <CacheKey>\n    <KeyFragment ref=\"request.uri\" type=\"string\"/>\n  </CacheKey>\n  <ExpirySettings>\n    <TimeoutInSeconds>{{TimeoutSeconds}}</TimeoutInSeconds>\n  </ExpirySettings>\n</ResponseCache>""",
+            """<ResponseCache name="{{PolicyName}}">
+  <CacheKey>
+    <KeyFragment ref="request.uri" type="string"/>
+  </CacheKey>
+  <ExpirySettings>
+    <TimeoutInSeconds>{{TimeoutSeconds}}</TimeoutInSeconds>
+  </ExpirySettings>
+</ResponseCache>""",
             ["PolicyName", "TimeoutSeconds"]),
 
         new PolicyTemplate(
             "RaiseFault",
             "Generates a custom error response.",
             "Mediation",
-            """<RaiseFault name="{{PolicyName}}">\n  <FaultResponse>\n    <Set>\n      <StatusCode>{{StatusCode}}</StatusCode>\n      <ReasonPhrase>{{ReasonPhrase}}</ReasonPhrase>\n    </Set>\n  </FaultResponse>\n</RaiseFault>""",
+            """<RaiseFault name="{{PolicyName}}">
+  <FaultResponse>
+    <Set>
+      <StatusCode>{{StatusCode}}</StatusCode>
+      <ReasonPhrase>{{ReasonPhrase}}</ReasonPhrase>
+    </Set>
+  </FaultResponse>
+</RaiseFault>""",
             ["PolicyName", "StatusCode", "ReasonPhrase"]),
 
         new PolicyTemplate(
             "ExtractVariables",
             "Extracts content from request/response into variables.",
             "Mediation",
-            """<ExtractVariables name="{{PolicyName}}">\n  <Source>request</Source>\n  <QueryParam name=\"{{QueryParam}}\">\n    <Pattern ignoreCase=\"true\">{{Pattern}}</Pattern>\n  </QueryParam>\n  <VariablePrefix>{{VariablePrefix}}</VariablePrefix>\n</ExtractVariables>""",
+            """<ExtractVariables name="{{PolicyName}}">
+  <Source>request</Source>
+  <QueryParam name="{{QueryParam}}">
+    <Pattern ignoreCase="true">{{Pattern}}</Pattern>
+  </QueryParam>
+  <VariablePrefix>{{VariablePrefix}}</VariablePrefix>
+</ExtractVariables>""",
             ["PolicyName", "QueryParam", "Pattern", "VariablePrefix"])
     ];
 
@@ -65,9 +98,9 @@ public sealed class PolicyTemplateRepository : IPolicyTemplateRepository
 
     public string GeneratePolicyXml(PolicyTemplate template, IDictionary<string, string> parameters)
     {
-        var sb = new StringBuilder(template.XmlContent);
+        var xml = template.XmlContent;
         foreach (var (key, value) in parameters)
-            sb.Replace($"{{{{{{key}}}}}}", value);
-        return sb.ToString();
+            xml = xml.Replace("{{" + key + "}}", value, StringComparison.Ordinal);
+        return xml;
     }
 }
