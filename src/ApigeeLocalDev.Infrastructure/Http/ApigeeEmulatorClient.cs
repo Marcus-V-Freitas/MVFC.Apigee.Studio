2cb06cbe12e5d2a78dc9dafb5a88e5c158160012
Marcus Costa <53195917+Marcus-V-Freitas@users.noreply.github.com>
2026-04-13 20:59:36 -0300

    fixes

--- src/ApigeeLocalDev.Infrastructure/Http/ApigeeEmulatorClient.cs
+++ src/ApigeeLocalDev.Infrastructure/Http/ApigeeEmulatorClient.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
 using ApigeeLocalDev.Domain.Entities;
 using ApigeeLocalDev.Domain.Interfaces;
